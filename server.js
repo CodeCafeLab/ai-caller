6ae3e8a284affc2db5a76717f717ac4d664805c4
Antima <antima@tezhire.com>
2025-08-28 13:20:35 +0530

    first

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your-very-secret-key'; // Use env 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root"||"aiuser",
-  password: process.env.DB_PASSWORD || ""||"AiCaller@1",
+  password: process.env.DB_PASSWORD || ""||"Antu@2252",
   database: process.env.DB_NAME || "ai-caller",
   multipleStatements: true // Allow multiple statements
 });
